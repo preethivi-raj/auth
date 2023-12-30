70203f71d74106b4bbcedcc0b388f7b0e66b1cce
Preethiviraj <20eumt057@skcet.ac.in>
2023-12-30 18:26:32 +0530

    Add a middle ware and function to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,15 @@
-import User from "../models/user.model.js"
-import bcryptjs from "bcryptjs"
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
+export const signup = async (req, res , next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-export const signup = async (req , res) =>{
-    const {username , email , password} = req.body;
-    const hashedPassword = bcryptjs.hashSync(password ,10)
-
-    const newUser = new User({username , email , password:hashedPassword})
-    try{
-        await newUser.save()
-    res.status(201).json({message:"User created Success"})
-    }
-    catch(err){
-        res.status(500).json(err.message )
-    }
-}
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created Success" });
+  } catch (err) {
+    next(err)
+  }
+};
--- api/index.js
+++ api/index.js
@@ -10,13 +10,26 @@ mongoose
 .connect(process.env.MONGO)
 .then(()=>{console.log("Connected to MongoDB")})
 .catch((err)=>{ console.log(err)})
+
 const app =express()
 
 app.use(express.json())
+app.use("/api/user",userRoutes)
+app.use("/api/auth" , authRoutes)
+
 app.listen(3000 ,()=>{
     console.log("Server run on port 3000")
 })
 
+app.use((err , req , res , next)=>{
+    const statusCode = err.statusCode || 500 ;
+    const message = err.message || "Internal Server Error";
 
-app.use("/api/user",userRoutes)
-app.use("/api/auth" , authRoutes)
+    return res.status(statusCode).json({
+        success : false ,
+        error : message,
+        statusCode :statusCode
+    })
+})
+
+
